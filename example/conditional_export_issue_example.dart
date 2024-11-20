2f85b9b5de6555dc071e80ce2c679b68805df2e1
mcmah309 <dillonmcmahon79@gmail.com>
2024-11-20 21:42:51 +0000

    base file

---
+++ example/conditional_export_issue_example.dart
@@ -0,0 +1,6 @@
+import 'package:conditional_export_issue/conditional_export_issue.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/conditional_export_issue_test.dart
@@ -0,0 +1,16 @@
+import 'package:conditional_export_issue/conditional_export_issue.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
