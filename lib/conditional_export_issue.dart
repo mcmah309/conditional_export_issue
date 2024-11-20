9d8bdcea38b3bf136197fe2929af19a0aa0876c3
mcmah309 <dillonmcmahon79@gmail.com>
2024-11-20 22:12:40 +0000

    windows first

--- lib/conditional_export_issue.dart
+++ lib/conditional_export_issue.dart
@@ -1,9 +1,9 @@
 library conditional_export_issue_test;
 
-export 'src/stub.dart' 
-  if (Platform.isLinux) 'src/linux.dart'
-  if (Platform.isWindows) 'src/windows.dart';
+// export 'src/stub.dart' 
+//   if (Platform.isLinux) 'src/linux.dart'
+//   if (Platform.isWindows) 'src/windows.dart';
 
 // export 'src/linux.dart' if (Platform.isWindows) 'src/windows.dart';
 
-// export 'src/windows.dart' if (Platform.isLinux) 'src/linux.dart';
+export 'src/windows.dart' if (Platform.isLinux) 'src/linux.dart';
